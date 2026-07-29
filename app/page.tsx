d328476f79012e695bcb6a133b5739bb9345a407
SaimonMaher <saimoonmaher15@gmail.com>
2026-07-29 10:44:15 +0000

    update confirmation page

--- app/page.tsx
+++ app/page.tsx
@@ -294,7 +294,12 @@ const availableDays = days.filter((day) => {
 
         </button>
 
-
+<button
+  onClick={() => router.push("/my-bookings")}
+  className="w-full border border-blue-700 text-blue-700 rounded-lg p-3 mt-3"
+>
+  استعلام عن حجوزاتي
+</button>
 
       </div>
 
@@ -302,11 +307,6 @@ const availableDays = days.filter((day) => {
     </main>
 
   );
-<button
-  onClick={() => router.push("/my-bookings")}
-  className="w-full border border-blue-700 text-blue-700 rounded-lg p-3 mt-3"
->
-  استعلام عن حجوزاتي
-</button>
+
 
 }
